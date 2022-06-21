3fc1a75c86f023cca3ed8cc3177986a53c70c555
zouhir <rajdaouizouhir.pro@gmail.com>
2022-06-21 20:24:47 +0100

    add apollo client plugin and runtime dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("kotlin-android")
     id("androidx.navigation.safeargs.kotlin")
+    id("com.apollographql.apollo3").version("3.3.2")
 }
 
 android {
@@ -32,19 +33,28 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.4.2")
     implementation("androidx.core:core-ktx:1.8.0")
+    // ui
+    implementation("androidx.appcompat:appcompat:1.4.2")
+    implementation("com.google.android.material:material:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
     implementation("androidx.recyclerview:recyclerview:1.2.1")
-    implementation("io.coil-kt:coil:1.4.0")
+
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
     implementation("androidx.navigation:navigation-fragment-ktx:2.4.2")
     implementation("androidx.navigation:navigation-ui-ktx:2.4.2")
+
     implementation("androidx.paging:paging-runtime-ktx:3.1.1")
-    implementation("com.google.android.material:material:1.6.1")
+
     implementation("androidx.security:security-crypto:1.1.0-alpha03")
+    implementation("om.apollographql.apollo3:apollo-runtime:3.3.2")
+    implementation("io.coil-kt:coil:1.4.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+}
+
+apollo {
+    packageName.set("com.example.rocketreserver")
 }
